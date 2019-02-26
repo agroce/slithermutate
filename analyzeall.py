5f08b58b45ba0843d83163a40afc1225511af4d1
agroce <agroce@gmail.com>
2019-02-26 15:50:24 -0700

    moved contracts

--- analyzeall.py
+++ analyzeall.py
@@ -1,32 +1,56 @@
+from __future__ import print_function
 import subprocess
 import glob
 import sys
+import scipy
+import scipy.stats
+import os
 
-for C in glob.glob("*.sol"):
-    if "mutant" in C:
-        continue # don't analyze the mutants themselves!
-    print "="*80
-    print "analyzing", C
+try:
+    os.mkdir("mutants")
+except:
+    pass
+
+scores = []
+
+for C in glob.glob("contracts/*.sol"):
+    print("="*80)
+    print("analyzing", C)
     with open("out.txt", 'w') as outf:
         num_base = subprocess.call(["slither", C, "--exclude-informational"], stdout=outf, stderr=outf)
-    print "ISSUES:", num_base
+    print("ISSUES:", num_base)
     sys.stdout.flush()    
     with open("out.txt", 'w') as outf:
-        r = subprocess.call(["mutate", C], stdout=outf, stderr=outf)
+        r = subprocess.call(["mutate", C, "--mutantDir", "mutants"], stdout=outf, stderr=outf)
     with open("out.txt", 'r') as outf:
         for line in outf:
             if "MUTANTS" in line:
-                print line,
+                print(line, end=" ")
+                if " VALID MUTANTS" in line:
+                    numMutants = int(line.split()[0])
     sys.stdout.flush()
+    if numMutants == 0:
+        print("NO VALID MUTANTS, SKIPPING...")
+        continue
     with open("out.txt", 'w') as outf:
-        subprocess.call(["analyze_mutants", C, "python maxissues.py " + str(num_base) + " " + C],
+        subprocess.call(["analyze_mutants", C, "python maxissues.py " + str(num_base) + " " + C,
+                             "--mutantDir", "mutants"],
                             stdout=outf, stderr=outf)
     with open("out.txt", 'r') as outf:
         for line in outf:
             if "MUTATION SCORE" in line:
-                print line,
+                print(line, end=" ")
+                score = float(line.split()[-1])
     subprocess.call(["wc","-l","killed.txt"])
     subprocess.call(["wc","-l","notkilled.txt"])
     sys.stdout.flush()    
     subprocess.call(["cp","killed.txt",C+".killed.txt"])
     subprocess.call(["cp","notkilled.txt",C+".notkilled.txt"])
+    scores.append(score)
+
+print("*"*80)
+print()
+print("SCORES:", scores)
+print("  MEAN:", scipy.mean(scores))
+print("  MEDIAN:", scipy.median(scores))
+print("  STD:", scipy.std(scores))
