0c884111f598a9790282190bafe27d4325c1c93a
agroce <agroce@gmail.com>
2019-03-18 11:10:36 -0700

    using cutoff and universalmutator utilities

--- mudiff.py
+++ mudiff.py
@@ -17,11 +17,13 @@ for f in glob.glob("424contracts/*.smartcheck.killed.txt"):
     for m in nk:
         if m not in nkslither:
             smartwins.append(readMutant(m, original, mutantDir="424mutants"))
-            
-ranked = FPF(smartwins, 10)
-print ranked
 
+ranked = FPF(smartwins, 100, cutoff=0.7)
+
+n = 0
 for (r, d) in ranked:
     print "*"*80
+    print "#" + str(n)
+    n += 1
     show(r)
     print "distance:", d
